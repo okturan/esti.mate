bd8e8e97e0bc1d26cc8f0301088ec1b64c9d8e2a
Okan Erturan <okanert@gmail.com>
2021-11-23 05:47:34 +0300

    Implemented a rounding algorithm

--- script.js
+++ script.js
@@ -35,68 +35,74 @@ const totals = document.getElementById("totals")
 
 document.addEventListener("change", updateTotals)
 
+// Rounding algorithm taken from stackoverflow 
+function roundToX(num, X) {
+  return +(Math.round(num + "e+"+X)  + "e-"+X);
+}
+
 function updateTotals() {
   totals.innerHTML = ""
   let sum = 0
   if (editing.checked) {
     let editingFee = editingRate.value * wordCount.value
-    editingFee = Math.round(editingFee)
+    editingFee = roundToX(editingFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Editing: ${editingFee} USD</li>`)
     sum += editingFee
   }
   if (extraction.checked) {
     let extractionFee = extractionRate.value * wordCount.value
-    extractionFee = Math.round(extractionFee)
+    extractionFee = roundToX(extractionFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Extraction: ${extractionFee} USD</li>`)
     sum += extractionFee
   }
   if (transcription.checked) {
     let transcriptionFee = transcriptionRate.value * recordingLength.value
-    transcriptionFee = Math.round(transcriptionFee)
+    transcriptionFee = roundToX(transcriptionFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Transcription: ${transcriptionFee} USD</li>`)
     sum += transcriptionFee
   }
   if (translation.checked) {
     let translationFee = translationRate.value * wordCount.value
-    translationFee = Math.round(translationFee)
+    translationFee = roundToX(translationFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Translation: ${translationFee} USD</li>`)
     sum += translationFee
   }
   if (cleanup.checked) {
     let cleanupFee = cleanupRate.value * wordCount.value
-    cleanupFee = Math.round(cleanupFee)
+    cleanupFee = roundToX(cleanupFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Cleanup: ${cleanupFee} USD</li>`)
     sum += cleanupFee
   }
   if (proofreading.checked) {
     let proofreadingFee = proofreadingRate.value * wordCount.value
-    proofreadingFee = Math.round(proofreadingFee)
+    proofreadingFee = roundToX(proofreadingFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Proofreading: ${proofreadingFee} USD</li>`)
     sum += proofreadingFee
   }
   if (voiceover.checked) {
     let voiceoverFee = voiceoverRate.value * wordCount.value
-    voiceoverFee = Math.round(voiceoverFee)
+    voiceoverFee = roundToX(voiceoverFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Voiceover: ${voiceoverFee} USD</li>`)
     sum += voiceoverFee
   }
   if (meeting.checked) {
     let meetingFee = meetingRate.value * meetingLength.value
-    meetingFee = Math.round(meetingFee)
+    meetingFee = roundToX(meetingFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Meeting: ${meetingFee} USD</li>`)
     sum += meetingFee
   }
   if (contentCreation.checked) {
     let contentCreationFee = contentCreationRate.value * wordCount.value
-    contentCreationFee = Math.round(contentCreationFee)
+    contentCreationFee = roundToX(contentCreationFee, 2)
     totals.insertAdjacentHTML("beforeend", `<li>Content Creation: ${contentCreationFee} USD</li>`)
     sum += contentCreationFee
   }
   let estimate = sum + sum * profitMargin.value / 100
   let profit = sum * profitMargin.value / 100
   totals.insertAdjacentHTML("beforeend", `<p><strong>Cost: ${sum} USD</strong></p>`)
-  totals.insertAdjacentHTML("beforeend", `<p><strong>Estimate: ${Math.round(estimate)} USD</strong></p>`)
-  totals.insertAdjacentHTML("beforeend", `<p><strong>Profit: ${Math.round(profit)} USD</strong></p>`)
+  totals.insertAdjacentHTML("beforeend", `<p><strong>Estimate: ${roundToX(estimate, 2)} USD</strong></p>`)
+  totals.insertAdjacentHTML("beforeend", `<p><strong>Profit: ${roundToX(profit, 2)} USD</strong></p>`)
 }
+
 
 updateTotals();
